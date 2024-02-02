5f1484f8e294af8273e07e884d92946e0bbd0b8c
Pranab0006 <158081782+Pranab0006@users.noreply.github.com>
2024-02-02 21:52:31 +0530

    Assignment

---
+++ Homework.py
@@ -0,0 +1,11 @@
+
+name = input("Enter your name: -")
+age = input("Enter your age: -")
+gender = input("Enter your gender: -")
+weight = input("Enter your weight: -")
+
+
+print(name)
+print(age)
+print(gender)
+print(weight)
